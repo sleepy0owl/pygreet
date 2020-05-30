e369fdb5aeb08ffec6281687b78b504ea4161c78
sourav.mondal <souravcristiano502@gmail.com>
2020-05-30 19:28:38 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name = 'pygreet',
+    version='0.0.1',
+    description='Private python package for testing',
+    url='',
+    author ='sourav mondal',
+    author_email='souravcristiano502@gmail.com',
+    license='unlicense',
+    packages=['pygreet'],
+    zip_safe=False
+)
